125278b9a7f2be5f82806bee22785993a7668065
Elodie Sayavong <sayavong.pro@gmail.com>
2021-09-15 14:38:54 +0200

    [crmDB]: backend, add protect.js middleware

---
+++ middlewares/protect.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+function protect(req, res, next) {
+    try {
+        const data = jwt.verify(req.cookie.jwt, process.env.JWT_SECRET);
+        req.cookie.jwtData = data;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Your token is not valid"
+        });
+    };
+};
+
+module.exports = protect;
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 // Import models
 const User = require("../models/usersModel");
+// Import middlewares
+const protect = require("../middlewares/protect");
 
 router.route("/")
     .post(async (req, res) => {
@@ -23,6 +25,13 @@ router.route("/")
 
         res.json({
             message: "Here is your cookie for subsequent requests !",
+        });
+    })
+    .get(protect, async (req, res) => {
+        console.log("This user come from _id: ", req.cookies.jwtData._id);
+
+        res.status(200).json({
+            message: "You are authorized",
         });
     });
 
